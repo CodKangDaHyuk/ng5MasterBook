0f1b4ad9354e005e6b4ad6c75aab83b2d668d5da
씨오디다혁 <kangdahyuk@codlabs.com>
2018-04-27 16:34:28 +0900

    ng generate component user-item

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule
