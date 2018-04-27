66dc61a1c65e75752d825370f2aeb4a71258c571
씨오디다혁 <kangdahyuk@codlabs.com>
2018-04-27 16:43:45 +0900

    npm install --save classlist.js

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserItemComponent } from './user-item/user-item.component';
-import { UserItemngComponent } from './user-itemng/user-itemng.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserItemComponent,
-    UserItemngComponent
+    UserItemComponent
+
   ],
   imports: [
     BrowserModule
